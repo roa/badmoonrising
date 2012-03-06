9d46aaa7bcd4bb5435878e775749dd361bbc2ad7
roa <roa@sisserl.(none)>
2012-03-06 19:19:05 +0100

    added select function

--- main.lua
+++ main.lua
@@ -9,15 +9,35 @@ local server = assert( socket.bind( "127.0.0.1", "9035" ) )
 
 local ip, port = server:getsockname()
 
+local selectlist = {}
+
 while 1 do
     local client = server:accept()
-    client:settimeout(10)
-    local line, err = client:receive()
-    if not err then
-        local getList = getRequest(line)
-        if getList[1] == "GET" then
-            client:send( answerRequest( getList[2] ) )
-        end
+    local ready = {}
+    local err = ""
+
+    
+    if client then
+        client:settimeout(1)
+        table.insert( selectlist, client )
     end
-    client:close()
+
+    list, _, err = socket.select( selectlist, nil, 1 )
+    
+    if err then print(err) end
+
+    for i, cli in ipairs(list) do
+
+        local line, err = client:receive()
+
+        if not err then
+            local getList = getRequest(line)
+            if getList[1] == "GET" then
+                client:send( answerRequest( getList[2] ) )
+            end
+        end
+
+        client:close()
+        table.remove( selectlist, i )
+    end
 end
